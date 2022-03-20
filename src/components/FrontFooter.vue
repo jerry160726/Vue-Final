a0e800e7f95414d7baec096af27c625e3400f43d
Jyen <jerry160726@gmail.com>
2022-03-20 23:30:23 +0800

    footer update

--- src/components/FrontFooter.vue
+++ src/components/FrontFooter.vue
@@ -1,93 +1,39 @@
 <template>
-  <div class="footer bg-dark">
-    <div class="container px-0 first-column d-lg-flex">
-      <h3 class="text-white me-lg-auto" data-aos="fade-right">
-        現在訂閱，獲得課程優惠碼！
-      </h3>
-
-      <div class="input-group" data-aos="fade-left">
-        <input
-          type="email"
-          class="form-control"
-          placeholder="輸入您的電子信箱"
-          aria-label="Input group example"
-          aria-describedby="btnGroupAddon"
-        />
-        <button type="submit" class="btn btn-light">訂閱</button>
-      </div>
-    </div>
-    <hr class="solid" />
-
-    <div class="container px-0 second-column" data-aos="fade-up">
+  <div class="footer bg-primary">
+    <div class="container second-column">
       <div class="weblink d-flex justify-content-between align-items-center">
-        <a href="index.html" class="d-inline-block">
-          <h4 class="text-white mb-2">DOYOGA</h4>
-        </a>
-
-        <ul class="list-unstyled d-flex weblink-icon">
-          <li>
-            <a href="#" class="d-inline-block pe-2"
-              ><i class="fab fa-facebook"></i
-            ></a>
-          </li>
-          <li>
-            <a href="#" class="d-inline-block pe-2"
-              ><i class="fab fa-instagram"></i
-            ></a>
-          </li>
-          <li>
-            <a href="#" class="d-inline-block"><i class="fab fa-line"></i></a>
-          </li>
-        </ul>
+        <router-link class="navbar-brand text-dark d-inline-block mb-2" to="/"
+          >小安甜點</router-link
+        >
       </div>
 
       <ul class="list-unstyled text-white">
-        <li class="mb-2">
-          <a href="#" class="d-inline-block footer-intro">課程介紹</a>
+        <li class="nav-item mb-2">
+          <router-link
+            class="nav-link text-dark d-inline-block footer-intro"
+            to="/products"
+            >產品頁面</router-link
+          >
         </li>
-        <li class="mb-2">
-          <a href="#" class="d-inline-block footer-space">瑜珈空間</a>
+        <li class="nav-item mb-2">
+          <router-link
+            class="nav-link text-dark d-inline-block footer-space"
+            to="/cart"
+            >購物車</router-link
+          >
         </li>
-
-        <div class="index-final-link d-md-flex">
-          <li class="mb-2">
-            <a href="#" class="d-inline-block footer-schedule">當月課表</a>
-          </li>
-          <span class="text-white">© 2021. All Rights Reserved.</span>
-        </div>
+        <li class="nav-item">
+          <router-link
+            class="nav-link text-dark d-inline-block footer-schedule"
+            to="/login"
+            >後台管理</router-link
+          >
+          <span class="text-dark copyright">© 2021. All Rights Reserved.</span>
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
-<style lang="scss" src="../assets/stylesheets/footer.scss" scoped>
+<style lang="scss" src="../assets/stylesheets/footer.scss">
 </style>
-
-<script>
-import emitter from '@/libs/emitter.js'
-
-export default {
-  data () {
-    return {
-      cartData: {
-        carts: [] // 清空購物車才可以用disabled
-      }
-    }
-  },
-  methods: {
-    getCart () {
-      this.$http
-        .get(`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/cart`)
-        .then((res) => {
-          this.cartData = res.data.data
-        })
-    }
-  },
-  mounted () {
-    this.getCart()
-    emitter.on('get-cart', () => {
-      this.getCart()
-    })
-  }
-}
-</script>
